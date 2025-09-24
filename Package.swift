0b839135f31a16116148e09f654965182bd40ad3
Pedro Santos <pedrohenriqueph004@gmail.com>
2025-09-24 17:29:05 -0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "C08_Package_G1",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "C08_Package_G1",
+            targets: ["C08_Package_G1"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "C08_Package_G1"),
+
+    ]
+)
